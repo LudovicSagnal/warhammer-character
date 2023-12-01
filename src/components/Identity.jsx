e47aba20d1c289df856777a4e98fa2b64253f361
LudovicSagnal <jiraipoint@gmail.com>
2023-12-01 20:10:45 +0100

    order in data, origin attribute working

--- src/components/Identity.jsx
+++ src/components/Identity.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Select from './Select';
 import { races, origins } from '../data/races';
 import { careers } from '../data/carreers';
+import { names_elf, names_human, firstnames_human, firstnames_elf } from '../data/names';
 
 const Identity = () => {
   const [race, setRace] = useState('humain');
@@ -19,35 +20,56 @@ const Identity = () => {
     { value: 'female', label: 'Femme' },
   ];
 
-  const handleRaceChange = (e) => {
-    console.log('Race:', e.target.value);
-    setRace(e.target.value);
-    if (e.target.value === 'humain') {
-      setOrigin('empire'); // Default origin for humans
-    } else {
-      setOrigin('all'); // Reset origin for other races
+  const getRandomFirstName = (race, origin, gender) => {
+    let names;
+    
+    if (race === 'humain') {
+      names = firstnames_human.filter(name => name.gender === gender && name.origin === origin);
+    } else if (race === 'elfe') {
+      names = firstnames_elf.filter(name => name.gender === gender);
     }
+    const randomIndex = Math.floor(Math.random() * names.length);
+    const selectedName = names[randomIndex]?.value || ''; 
+    
+    return selectedName;
   };
-
-  const handleOriginChange = (e) => {
-    setOrigin(e.target.value);
+  const handleRandomFirstName = (e) => {
+    e.preventDefault();
+    const randomFirstName = getRandomFirstName(race, origin, gender);
+    setFirstname(randomFirstName);
   };
 
   return (
     <div>
       <h2>Identité</h2>
-      <Select name="race" options={races} label={'Choisissez une race'} onValueChange={handleRaceChange} setValue={setRace} />
+      <Select name="race" options={races} label={'Choisissez une race'} onValueChange={(e) => setRace(e.target.value)} setValue={setRace} />
       {race === 'humain' && (
-        <Select name="origin" options={origins} label={'Choisissez une origine'} onValueChange={handleOriginChange} setValue={setOrigin} />
+        <Select name="origin" options={origins} label={'Choisissez une origine'} onValueChange={(e) => setOrigin(e.target.value)} setValue={setOrigin} />
       )}
       <Select name="gender" options={genderChoice} label={'Choisissez un genre'} onValueChange={(e) => setGender(e.target.value)} setValue={setGender} />
 
       <h2>Détails du personnage</h2>
-      <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
-      <input type="text" name="firstname" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
-      <input type="number" name="age" value={age} onChange={(e) => setAge(e.target.value)} />
-      <input type="number" name="height" value={height} onChange={(e) => setHeight(e.target.value)} />
-      <input type="number" name="weight" value={weight} onChange={(e) => setWeight(e.target.value)} />
+      <div>
+        <label htmlFor="name">Nom</label>
+        <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />      
+      </div>
+      <div>
+        <label htmlFor="firstname">Prénom</label>
+        <input type="text" name="firstname" value={firstname} onChange={(e) => setFirstname(e.target.value)} />  
+        <button onClick={handleRandomFirstName}>Générer un nom aléatoire</button>        
+      </div>
+      <div>
+        <label htmlFor="age">Age</label>
+        <input type="number" name="age" value={age} onChange={(e) => setAge(e.target.value)} />        
+      </div>
+      <div>
+        <label htmlFor="height">Taille</label>
+        <input type="number" name="height" value={height} onChange={(e) => setHeight(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="weight">Poids</label>
+        <input type="number" name="weight" value={weight} onChange={(e) => setWeight(e.target.value)} />        
+      </div>
       <Select name="careers" options={careers} onValueChange={(e) => setCareer(e.target.value)} setValue={setCareer} />
     </div>
   );
--- src/data/names.js
+++ src/data/names.js
@@ -1,52 +1,128 @@
-const names_human = [
+
+/////////////////////////////////   HUMAINS   ////////////////////////////////////
+
+const firstnames_human = [
+
 //Empire
 
     //male
-    { value: 'Hans', origin: 'empire', genre: 'male' },
-    { value: 'Luthor', origin: 'empire', genre: 'male' },
+    { value: 'Hans', origin: 'empire', gender: 'male' },
+    { value: 'Luthor', origin: 'empire', gender: 'male' },
     //female
-    { value: 'Hanna', origin: 'empire', genre: 'female' },
-    { value: 'Frida', origin: 'empire', genre: 'female' },
+    { value: 'Hanna', origin: 'empire', gender: 'female' },
+    { value: 'Frida', origin: 'empire', gender: 'female' },
 
 //Bretonnia
 
     //male
-    { value: 'Louis', origin: 'bret', genre: 'male' },
-    { value: 'Louen', origin: 'bret', genre: 'male' },
+    { value: 'Louis', origin: 'bretonien', gender: 'male' },
+    { value: 'Louen', origin: 'bretonien', gender: 'male' },
     //female
-    { value: 'Jeanne', origin: 'bret', genre: 'female' },
-    { value: 'Marie', origin: 'bret', genre: 'female' },
+    { value: 'Jeanne', origin: 'bretonien', gender: 'female' },
+    { value: 'Marie', origin: 'bretonien', gender: 'female' },
+
+//Estalia
+
+    //male
+    { value: 'Diego', origin: 'estalien', gender: 'male' },
+    { value: 'Ramon', origin: 'estalien', gender: 'male' },
+    //female
+    { value: 'Consuela', origin: 'estalien', gender: 'female' },
+    { value: 'Maria', origin: 'estalien', gender: 'female' },
+
+//Tilea
+
+    //male
+    { value: 'Vincenzo', origin: 'tiléen', gender: 'male' },
+    { value: 'Giovanni', origin: 'tiléen', gender: 'male' },
+    //female
+    { value: 'Lucia', origin: 'tiléen', gender: 'female' },
+    { value: 'Giulia', origin: 'tiléen', gender: 'female' },
 
 //Kislev
 
     //male
-    { value: 'Ivan', origin: 'kislev', genre: 'male' },
-    { value: 'Dmitri', origin: 'kislev', genre: 'male' },
+    { value: 'Ivan', origin: 'kislévite', gender: 'male' },
+    { value: 'Dmitri', origin: 'kislévite', gender: 'male' },
     //female
-    { value: 'Lena', origin: 'kislev', genre: 'female' },
-    { value: 'Olga', origin: 'kislev', genre: 'female' },
+    { value: 'Lena', origin: 'kislévite', gender: 'female' },
+    { value: 'Olga', origin: 'kislévite', gender: 'female' },
+    { value: 'Ekatarina', origin: 'kislévite', gender: 'female' },
 
 //Norsca
 
     //male
-    { value: 'Björn', origin: 'norsca', genre: 'male' },
-    { value: 'Hakkon', origin: 'norsca', genre: 'male' },
-    { value: 'Ludvik', origin: 'norsca', genre:'male'},
+    { value: 'Björn', origin: 'norse', gender: 'male' },
+    { value: 'Hakkon', origin: 'norse', gender: 'male' },
+    { value: 'Ludvik', origin: 'norse', gender:'male'},
     //female
-    { value: 'Astrid', origin: 'norsca', genre: 'female' },
-    { value: 'Helga', origin: 'norsca', genre: 'female' },
+    { value: 'Astrid', origin: 'norse', gender: 'female' },
+    { value: 'Helga', origin: 'norse', gender: 'female' },
 
 ];
+const names_human = [
+
+//Empire
+
+    //male
+    { value: 'Hans', origin: 'empire', gender: 'male' },
+    { value: 'Luthor', origin: 'empire', gender: 'male' },
+    //female
+    { value: 'Hanna', origin: 'empire', gender: 'female' },
+    { value: 'Frida', origin: 'empire', gender: 'female' },
+
+//Bretonnia
+
+    //male
+    { value: 'Louis', origin: 'bretonien', gender: 'male' },
+    { value: 'Louen', origin: 'bretonien', gender: 'male' },
+    //female
+    { value: 'Jeanne', origin: 'bretonien', gender: 'female' },
+    { value: 'Marie', origin: 'bretonien', gender: 'female' },
+
+//Kislev
+
+    //male
+    { value: 'Ivan', origin: 'kislévite', gender: 'male' },
+    { value: 'Dmitri', origin: 'kislévite', gender: 'male' },
+    //female
+    { value: 'Lena', origin: 'kislévite', gender: 'female' },
+    { value: 'Olga', origin: 'kislévite', gender: 'female' },
+
+//Norsca
+
+    //male
+    { value: 'Björn', origin: 'norse', gender: 'male' },
+    { value: 'Hakkon', origin: 'norse', gender: 'male' },
+    { value: 'Ludvik', origin: 'norse', gender:'male'},
+    //female
+    { value: 'Astrid', origin: 'norse', gender: 'female' },
+    { value: 'Helga', origin: 'norse', gender: 'female' },
+
+];
+
+/////////////////////////////////////   ELFES   //////////////////////////////////////
 
 const names_elf = [
 
     //male
-    { value: 'Torrendil', genre: 'male' },
-    { value: 'Lorenor', genre: 'male' },
-    { value: 'Torrendil', genre: 'male' },
+    { value: 'Torrendil', gender: 'male' },
+    { value: 'Lorenor', gender: 'male' },
+    { value: 'Torrendil', gender: 'male' },
     //female
-    { value: 'Liandra', genre: 'female' },
-    { value: 'Ilona', genre: 'female' },
+    { value: 'Liandra', gender: 'female' },
+    { value: 'Ilona', gender: 'female' },
 ];
 
-export { names_human, names_elf };
+const firstnames_elf = [
+
+    //male
+    { value: 'Torrendil', gender: 'male' },
+    { value: 'Lorenor', gender: 'male' },
+    { value: 'Torrendil', gender: 'male' },
+    //female
+    { value: 'Liandra', gender: 'female' },
+    { value: 'Ilona', gender: 'female' },
+];
+
+export { names_human, names_elf, firstnames_human, firstnames_elf };
