cacbd3a3964b583b482bdb3092e31a5d373429a8
LudovicSagnal <jiraipoint@gmail.com>
2024-02-27 15:48:31 +0100

    pdf completed 75%

--- src/components/PDFDocument.jsx
+++ src/components/PDFDocument.jsx
@@ -5,7 +5,7 @@ import autoTable from 'jspdf-autotable';
 
 const PDFDocument = () => {
   const {
-    race, origin, gender, selectedCareer, selectedCareerName, name, firstname, age, height, weight, eyeColor, hairColor, birthplace, siblings, marks, astral, choosenPortrait } = useContext(RaceContext);
+    race, origin, gender, selectedCareer, selectedCareerName, name, firstname, age, height, weight, eyeColor, hairColor, birthplace, siblings, marks, astral, choosenPortrait, selectedSkills, selectedTalents, selectedEquipment } = useContext(RaceContext);
 
     const canvasRef = useRef(null);
 
@@ -37,19 +37,26 @@ const PDFDocument = () => {
 
       const infoLines = [`Age : ${age} ans` ,`Taille : ${height} m` ,`Poids : ${weight} kg` ,`Yeux : ${eyeColor}` ,`Cheveux : ${hairColor}` , `Lieu de naissance : ${birthplace}`, `Nb de frères et soeurs : ${siblings}`, `Signes distinctifs : ${marks}`, `Signe astral : ${astral}`];
 
-      const skillsLines = [`Compétences : ${selectedCareer.skills.mandatory}`]
+      const skillsLines = [`${selectedCareer.skills.mandatory}, ${selectedSkills}`]
+      const talentsLines = [`${selectedCareer.talents.mandatory}, ${selectedTalents}`]
+      const equipmentLines = [`${selectedCareer.dotation.mandatory}, ${selectedEquipment}`]
 
       addMultipleLines(pdf, profilLines, 10, verticalPosition, lineHeight);
       if (race === 'humain') {
         pdf.text(`Origine: ${origin}`, 10, 50);
       }
       pdf.text(careerLine, 100, 70);
+      pdf.text('Compétences', 10, 140);
+      pdf.text('Talents', 10, 160);
+      pdf.text('Equipement', 10, 180);
 
       pdf.setFontSize(8);
 
       addMultipleLines(pdf, infoLines, 10, 60, lineHeight-5);
 
-      (selectedCareer != null && addMultipleLines(pdf, skillsLines, 10, 140));
+      (selectedCareer != null && addMultipleLines(pdf, skillsLines, 10, 150));
+      (selectedCareer != null && addMultipleLines(pdf, talentsLines, 10, 170));
+      (selectedCareer != null && addMultipleLines(pdf, equipmentLines, 10, 190));
 
       pdf.autoTable({ html: '#profil-stats', theme: 'grid', margin: {top: 80, left: 70}, headStyles: {
         fillColor: [120,61,15],
