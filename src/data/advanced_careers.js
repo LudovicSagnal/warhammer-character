cbcb5af22af2decb746dbb9007cb0f24bce56674
LudovicSagnal <jiraipoint@gmail.com>
2024-05-06 22:44:15 +0200

    new advanced career

--- src/data/advanced_careers.js
+++ src/data/advanced_careers.js
@@ -139,6 +139,79 @@ const advanced_careers = [
     },
   },
   {
+    name: `Aubergiste`,
+    description: `Les Aubergistes possèdent et dirigent des établissements qui assurent la prospérité économique du Vieux Monde. Beaucoup de familles d'Aubergistes possèdent leur établissement depuis des générations. Ils satisfont aux besoins de tous les clients, quel que soit leur statut social, du moins tant qu'ils payent. Aujourd'hui, comme les gens ne voyagent plus trop, les Aubergistes ont compris qu'une information vaut parfois de l'or et ils apprécient d'autant plus les nouvelles venues de l'étranger. Certains Aubergistes arrondissent leurs fins de mois en jouant les intermédiaires entre leurs clients. Leur travail consiste alors à trouver de la compagnie à leur clientèle ou à mettre en contact des gens aux intérêts communs.`,
+    skills: {
+      mandatory: [
+        `Charisme`,
+        `Commérage`,
+        `Connaissances générales (Empire)`,
+        `Evaluation`,
+        `Marchandage`,
+        `Métier (cuisinier)`,
+        `Perception`,
+        `Résistance à l'alcool`,
+      ],
+      choice: [
+        {
+          options:[`Baratin`, `Lecture sur les lèvres`],
+          quantity: 1,
+        },
+        {
+          options:[`Escamotage`, `Lire/écrire`],
+          quantity: 1,
+        },
+        {
+          options:[`Langue (bretonnien)`, `Langue (kislevien)`, `Langue (reikspiel)`, `Langue (tiléen)`],
+          quantity: 1,
+        },
+      ]
+    },
+    talents: {
+      mandatory: [
+        `Coups assommants`,
+      ],
+      choice: [
+        {
+          options:[`Code de la rue`, `Etiquette`],
+          quantity: 1,
+        },
+        {
+          options:[`Combat de rue`, `Dur en affaires`],
+          quantity: 1,
+        },
+      ],
+    },
+    dotation: {
+      mandatory: [
+        `Auberge`,
+        `Un ou plusieurs serviteurs`,
+      ],
+    },
+    access: [
+      `Bourgeois`,
+      `Serviteur`,
+    ],
+    advanced: [
+      `Bourgeois`,
+      `Contrebandier`,
+      `Hors-la-loi`,
+      `Marchand`,
+      `Receleur`,
+    ],
+    stats: {
+      CC: 10,
+      CT: 5,
+      F: 5,
+      E: 10,
+      Ag: 20,
+      Int: 10,
+      FM: 10,
+      Soc: 20,
+      B: 4,
+    },
+  },
+  {
     name: `Charlatan`,
     description: `Les Charlatans sont des filous, d'extraordinaires menteurs capables de convaincre autrui de n'importe quoi. Leur langue bien pendue et leur ingéniosité leur permettent de manipuler les individus incrédules et de prendre la fuite avec leur argent. Un Charlatan ment comme il respire. La plupart se contentent de vendre des remèdes miracles et autres babioles sans valeur censés contrer les effets de la magie noire. Cependant, d'autres vont jusqu'à vendre des biens qui ne leur appartiennent pas. Les plus grands Charlatans montent des escroqueries qui plument complètement de riches nobles ou marchands.`,
     skills: {
